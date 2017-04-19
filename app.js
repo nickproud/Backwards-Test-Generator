0457e2fb68aa475d32d93dd948622042b2b1e298
Nick <nickproud@outlook.com>
2017-04-19 23:30:35 +0100

    bug fix
    Issue with reference to button

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-
+//Text reveral function
 function reverseText() {
     let textInput = document.getElementById("inputBox").value;
     let inputArray = [];
     for (let i = 0; i < textInput.length; i++) {
 
         inputArray.push(textInput[i]);
-        
-
     }
+  
     console.log("Array before reversal will be " + inputArray);
     inputArray.reverse();
     console.log("Array after reversal will ve " + inputArray);
@@ -18,16 +17,8 @@ function reverseText() {
         result = result + inputArray[i];
     };
     console.log("Reversed result is " + result);
-    alert(result);
-
-    
-
-
-    //let result = inputArray.join();
-    //alert(result);
-
-   
+    alert(result); 
 };
-
+//target the 'reverse button and execute reversal function when clicked
 let reverseBtn = document.getElementById("reverseBtn");
 reverseBtn.addEventListener("click", reverseText);
