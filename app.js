3c51681147a44b2acc4b76460fb1adc7b98a8ab0
Nick <nickproud@outlook.com>
2017-04-19 23:24:12 +0100

    Implemented reversal logic
    app.js now reverses text and outputs as an alert box. 
    logging sent to console also

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 
+function reverseText() {
+    let textInput = document.getElementById("inputBox").value;
+    let inputArray = [];
+    for (let i = 0; i < textInput.length; i++) {
+
+        inputArray.push(textInput[i]);
+        
+
+    }
+    console.log("Array before reversal will be " + inputArray);
+    inputArray.reverse();
+    console.log("Array after reversal will ve " + inputArray);
+    let result = "";
+
+    for (let i = 0; i < inputArray.length; i++) {
+
+        result = result + inputArray[i];
+    };
+    console.log("Reversed result is " + result);
+    alert(result);
+
+    
+
+
+    //let result = inputArray.join();
+    //alert(result);
+
+   
+};
+
+let reverseBtn = document.getElementById("reverseBtn");
+reverseBtn.addEventListener("click", reverseText);
