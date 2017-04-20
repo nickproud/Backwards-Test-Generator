3ce60a5a48189a7880db8d8950c162374ecbc133
Nick <nickproud@outlook.com>
2017-04-20 01:08:05 +0100

    Reset Page Functionality Added
    Page can now be reset.

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const reverseBtn = document.getElementById("reverseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const pageBody = document.querySelector("body");
+const outputHead = document.getElementById("outputHeader");
+
 //Text reveral function
 function reverseText() {
     let textInput = document.getElementById("inputBox").value;
@@ -16,15 +21,45 @@ function reverseText() {
 
         result = result + inputArray[i];
     };
+
     console.log("Reversed result is " + result);
     let outputDiv = document.getElementById("textareaDiv");
     let outputBox = document.createElement("textarea");
+    let resetBtn = document.createElement("button");
+    resetBtn.textContent = "Reset";
+    resetBtn.id = "resetBtn";
+    
     outputBox.id = "outputTextArea";
-    outputBox.rows = "20";
+    outputBox.rows = "10";
     outputBox.cols = "100";
     outputBox.value = result;
-    outputDiv.appendChild(outputBox);
+    outputHead.style.display = "block";
+    reverseBtn.style.display = "none";
+    pageBody.appendChild(outputBox);
+    pageBody.appendChild(resetBtn);
+    resetBtn.addEventListener("click", resetPage);
+  
+
+   
+    
 };
-//target the 'reverse button and execute reversal function when clicked
-let reverseBtn = document.getElementById("reverseBtn");
+
+function resetPage() {
+    let pageBody = document.querySelector("body");
+    let outputBox = document.getElementById("outputTextArea");
+    pageBody.removeChild(outputBox);
+    let resetButton = document.getElementById("resetBtn");
+    pageBody.removeChild(resetButton);
+    outputHead.style.display = "none";
+    reverseBtn.style.display = "block";
+    let inputbox = document.getElementById("inputBox");
+    inputbox.value = "";
+   
+}
+
+
+
 reverseBtn.addEventListener("click", reverseText);
+
+
+
