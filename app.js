cae3c2ee23b45e6ceb0dbbb3f855cef1b24e62f5
Nick <nickproud@outlook.com>
2017-04-20 01:27:18 +0100

    Blank value prevention
    Application now prevents insertion of blank values

--- app.js
+++ app.js
@@ -3,45 +3,49 @@ const resetBtn = document.getElementById("resetBtn");
 const pageBody = document.querySelector("body");
 const outputHead = document.getElementById("outputHeader");
 
-//Text reveral function
+
 function reverseText() {
     let textInput = document.getElementById("inputBox").value;
-    let inputArray = [];
-    for (let i = 0; i < textInput.length; i++) {
 
-        inputArray.push(textInput[i]);
-    }
-  
-    console.log("Array before reversal will be " + inputArray);
-    inputArray.reverse();
-    console.log("Array after reversal will be " + inputArray);
-    let result = "";
+        let inputArray = [];
+        for (let i = 0; i < textInput.length; i++) {
 
-    for (let i = 0; i < inputArray.length; i++) {
+            inputArray.push(textInput[i]);
+        }
 
-        result = result + inputArray[i];
-    };
+        console.log("Array before reversal will be " + inputArray);
+        inputArray.reverse();
+        console.log("Array after reversal will be " + inputArray);
+        let result = "";
 
-    console.log("Reversed result is " + result);
-    let outputDiv = document.getElementById("textareaDiv");
-    let outputBox = document.createElement("textarea");
-    let resetBtn = document.createElement("button");
-    resetBtn.textContent = "Reset";
-    resetBtn.id = "resetBtn";
-    
-    outputBox.id = "outputTextArea";
-    outputBox.rows = "10";
-    outputBox.cols = "100";
-    outputBox.value = result;
-    outputHead.style.display = "block";
-    reverseBtn.style.display = "none";
-    pageBody.appendChild(outputBox);
-    pageBody.appendChild(resetBtn);
-    resetBtn.addEventListener("click", resetPage);
-  
+        for (let i = 0; i < inputArray.length; i++) {
 
-   
-    
+            result = result + inputArray[i];
+        };
+
+
+        if (textInput == "") {
+            alert("cannot insert a blank value");
+        }
+        else {
+            console.log("Reversed result is " + result);
+            let outputDiv = document.getElementById("textareaDiv");
+            let outputBox = document.createElement("textarea");
+            let resetBtn = document.createElement("button");
+            resetBtn.textContent = "Reset";
+            resetBtn.id = "resetBtn";
+
+            outputBox.id = "outputTextArea";
+            outputBox.rows = "10";
+            outputBox.cols = "100";
+            outputBox.value = result;
+            outputHead.style.display = "block";
+            reverseBtn.style.display = "none";
+            pageBody.appendChild(outputBox);
+            pageBody.appendChild(resetBtn);
+            resetBtn.addEventListener("click", resetPage);
+        }
+
 };
 
 function resetPage() {
@@ -56,7 +60,6 @@ function resetPage() {
     inputbox.value = "";
    
 }
-
 
 
 reverseBtn.addEventListener("click", reverseText);
